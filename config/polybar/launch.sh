016e220672dfc9038983d031bcd6fe3263c36e31
Fottotex <96926571+xFottotex@users.noreply.github.com>
2025-05-27 19:03:18 +0200

    Add files via upload

--- config/polybar/launch.sh
+++ config/polybar/launch.sh
@@ -3,9 +3,7 @@
 killall -q polybar
 
 # Lanuch bar
-echo "---" | tee -a /tmp/polybar1.log /tmp/polybar2.log /tmp/polybar3.log
-polybar mybar 3>&1 | tee -a /tmp/polybar1.log & disown
-polybar mybar2 3>&1 | tee -a /tmp/polybar2.log & disown
-polybar mybar3 3>&1 | tee -a /tmp/polybar3.log & disown
+echo "---" | tee -a /tmp/polybar1.log
+polybar mybar 2>&1 | tee -a /tmp/polybar1.log & disown
 
 echo "Bars launched..."
